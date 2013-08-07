704d0c3648b7bdd0acd050848e041253807e0a5c
jacobraim <jacob.raim@gmail.com>
2013-08-07 17:34:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function LoadScripts() {
 
 function addToGameList(gameInfo) {
     var element = $("<tr class='gameInfo'><td>" + gameInfo.opponent + "</td><td>" + gameInfo.date + "</td><td>" + gameinfo.gameType + "</td></tr>")
-    var gameId = "" + gameInfo.opponent + "_" + gameInfo.date.replace(/\//g,'')
+    var gameId = "" + gameInfo.opponent + "_" + gameInfo.date + "_" + gameinfo.gameType.replace(/\//g,'')
     element.attr('id',gameId)
     element.data('gameInfo', gameInfo)
     element.data('gameInfo').gameId = gameId
